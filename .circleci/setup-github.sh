fd7d097e926424c9b95052dba5fa011f10d67b6a
Jason Thai <thaixtri@gmail.com>
2019-07-21 09:40:55 -0700

    switch to source branch

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -10,6 +10,6 @@ rm -R _site/
 
 git add -fA
 git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
-git push -f origin master
+git push --force --quiet origin master
 
 echo "deployed successfully"
