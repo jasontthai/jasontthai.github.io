902362c89a6a7f311eb7a2bd4ce835fff8e8e20b
zirius <thaixtri@gmail.com>
2019-08-06 23:08:57 +0000

    "c8f5011 - workbox-config.js"

--- sw.js
+++ sw.js
@@ -213,5 +213,30 @@ workbox.precaching.precacheAndRoute([
   }
 ])
 
-workbox.routing.registerRoute(/(\.js$|\.css$)/, new workbox.strategies.CacheFirst());
-workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, new workbox.strategies.StaleWhileRevalidate());
+// Caching Images
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+// Cache CSS and JavaScript Files
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
+);
+
+// Caching Content from Multiple Origins
+workbox.routing.registerRoute(
+  /.*(?:googleapis|gstatic)\.com/,
+  new workbox.strategies.StaleWhileRevalidate(),
+);
