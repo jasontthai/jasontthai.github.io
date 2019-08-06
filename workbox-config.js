67dfa6ed11aeeea5dad890bf4c5a009119128b95
zirius <thaixtri@gmail.com>
2019-08-06 21:59:42 +0000

    "5d33eb2 - update config.yml"

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "_site/",
+  "globPatterns": [
+    "**/*.{html,txt,css,png,webp,jpg,js,json,svg,md,ico,xml}"
+  ],
+  "swDest": "_site/sw.js",
+  "swSrc": "sw.js"
+};
